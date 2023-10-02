ab1448b4875f2cc53c170181d06600c4355bddb5
Elad Hamneshin <69572691+EladHamneshin@users.noreply.github.com>
2023-10-02 11:14:25 +0000

    {update} logo to svg + usage'

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,8 +1,10 @@
+
+import Logo from '../assets/logo.svg';
+//#0e98da
 const HomePage = () => {
-    return (
-        <div>
-            <h1>Home Page</h1>
-      </div>);
+    return (<div style={{backgroundColor:'#0e98da'}}>
+        <img width={200} height={200} src={Logo} alt='logo'/> 
+        </div>);
 }
 
 export default HomePage;
