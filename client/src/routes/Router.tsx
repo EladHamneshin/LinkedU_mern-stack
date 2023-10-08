a4e7967bfcbb205798f4f692d66b9367fd8e0cb7
Elad Hamneshin <69572691+EladHamneshin@users.noreply.github.com>
2023-10-08 08:46:47 +0000

    {update} change pages names to xPage

--- client/src/routes/Router.tsx
+++ client/src/routes/Router.tsx
@@ -1,17 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import ROUTES from "./routesModel";
-import Home from "../pages/Home";
-import Login from "../pages/Login";
-import Register from "../pages/Register";
+import HomePage from "../pages/HomePage";
+import LoginPage from "../pages/LoginPage";
+import RegisterPage from "../pages/RegisterPage";
 import App from "../App";
 
 const Router = () => {
     return (
         <Routes>
             <Route path={'/'} element={<App />}>
-                <Route index={true} path={ROUTES.HOME} element={<Home />} />
-                <Route path={ROUTES.LOGIN} element={<Login/>} />
-                <Route path={ROUTES.REGISTER} element={<Register/>} />
+                <Route index={true} path={ROUTES.HOME} element={<HomePage />} />
+                <Route path={ROUTES.LOGIN} element={<LoginPage/>} />
+                <Route path={ROUTES.REGISTER} element={<RegisterPage/>} />
             </Route>
             <Route path={ROUTES.DEFAULT} element={<h1>404 Not Found</h1>} />
         </Routes>
